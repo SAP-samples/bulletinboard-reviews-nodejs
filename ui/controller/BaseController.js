23f0fe3dad99d585227dd2445902c347028e377b
D068017 <marcel.dambach@sap.com>
2019-08-16 13:41:21 +0200

    further cleanup

--- ui/controller/BaseController.js
+++ ui/controller/BaseController.js
@@ -37,40 +37,6 @@ sap.ui.define([
 			},
 			
 			/**
-			 * Convenience method for setting the view model in every controller of the application.
-			 * @public
-			 * @param {string} sURL location of model data
-			 * @param {object} oParameters parameters for retrieving model data
-			 * @param {string} sName the model name
-			 * @returns {sap.ui.mvc.View} the view instance
-			 */
-			updateModel : function (sURL, oParameters, sName) {
-				var oNewModel = new sap.ui.model.json.JSONModel();
-				oNewModel.attachRequestCompleted(function() {
-					this.getView().getModel(sName).setData(oNewModel.getData(), false);
-				}, this);
-				oNewModel.loadData(sURL, oParameters);
-				return this.getView();
-			},			
-
-			/**
-			 * Convenience method for handling asynchronous model loading.
-			 * @public
-			 * @param {function} fCallback function to call when model data is available
-			 * @param {object} oListener Object on which to call the given function. If empty, this controller is used.
-			 * @returns {null} nothing
-			 */
-			waitForModel : function (fCallback, oListener) {
-				oListener = oListener || this;
-				var oModelData = this.getModel().getData()["value"];
-				if (oModelData === undefined || !Array.isArray(oModelData)) {
-					this.getModel().attachRequestCompleted(fCallback, oListener);
-				} else {
-					fCallback.call(oListener);
-				}
-			},			
-
-			/**
 			 * Convenience method for getting the resource bundle.
 			 * @public
 			 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
@@ -99,33 +65,8 @@ sap.ui.define([
 				return this.getOwnerComponent().isLocalDevEnvironment();
 			},
 
-			getUserId : function() {
-				return this.getModel("user").getProperty("/id");
-			},
-
 			getMainServiceURL : function() {
 				return this.getOwnerComponent().getMetadata().getManifestEntry("sap.app").dataSources["mainService"].uri;
-			},
-			
-			deleteAd : function(iAdId, fSuccess) {
-				this.fetchCsrfToken(iAdId, this._deleteAd.bind(this), fSuccess);
-			},
-			
-			_deleteAd : function(iAdId, sCsrfToken, fSuccess) {
-				$.ajax({
-					method : "DELETE",
-					url: this.getMainServiceURL() + "/" + iAdId,
-					// Pseudo-authentication to qualify as "premium user" who is
-					// allowed to create new reviews
-					headers : { "User-Id" : this.getUserId(), "x-csrf-token" : sCsrfToken }
-				})
-				.done(fSuccess)
-				.fail( function(oJqXHR, sTextStatus, sErrorThrown) {
-						MessageToast.show("Failed to delete the ad.");
-						jQuery.sap.log.error("Failed to delete ad " + iAdId + ".", sErrorThrown);
-		
-					}
-				);
 			},
 			
 			fetchCsrfToken : function(oData, fCallback, fSuccess) {
@@ -140,23 +81,6 @@ sap.ui.define([
 					MessageToast.show("Failed to fetch x-csrf-token.");
 					jQuery.sap.log.error("Failed to fetch x-csrf-token.", sErrorThrown);
 				});
-			},
-			
-			/**
-			 * Locate ad in array by its ID
-			 * @param iAdId: ID of the ad to locate
-			 * @param aAllAds: array of all ads
-			 */
-			_getAdIndexInModel : function(iAdId, aAllAds) {
-				var iAdIndex;
-				for (var i = 0; i < aAllAds.length; i++) {
-				    if (aAllAds[i].id == iAdId) {
-				    	iAdIndex = i;
-				    	break;
-				    } 
-				}
-
-				return iAdIndex;
 			}
 
 		});
