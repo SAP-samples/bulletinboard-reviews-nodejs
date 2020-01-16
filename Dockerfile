ca0128243225d235e34567b20d71f8da4e982d88
D068017 <marcel.dambach@sap.com>
2020-01-16 11:27:43 +0100

    Dockerize and adjust app

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:10.18-alpine3.10
+
+WORKDIR /app
+COPY package*.json ./
+COPY js ./js
+COPY ui ./ui
+
+RUN npm ci
+
+EXPOSE 9090
+ENTRYPOINT [ "npm"]
+CMD [ "start" ]
--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@
 const ExpressServer = require('./express-server')
 const PostgresReviewsService = require('./postgres-reviews-service')
 
-const VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES)
-const DB_CONNECTION_URI = VCAP_SERVICES.postgresql[0].credentials.uri
+// const VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES)
+// const DB_CONNECTION_URI = VCAP_SERVICES.postgresql[0].credentials.uri
+const DB_CONNECTION_URI = process.env.POSTGRES_URI
 
 const server = new ExpressServer(new PostgresReviewsService(DB_CONNECTION_URI))
 server.start(process.env.PORT)
