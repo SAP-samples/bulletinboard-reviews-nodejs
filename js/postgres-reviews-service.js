cdc11e22855338a30b17c458d25ca023272ed176
D068017 <marcel.dambach@sap.com>
2020-01-27 11:15:38 +0100

    Remove semicolons and sync sql syntax

--- js/postgres-reviews-service.js
+++ js/postgres-reviews-service.js
@@ -5,38 +5,38 @@ const pg = require('pg')
 function PostgresReviewsService(dbConnectionUri) {
 
     const pool = new pg.Pool({ 'connectionString': dbConnectionUri })
-    const sql = 'CREATE TABLE IF NOT EXISTS "reviews"("reviewee_email" VARCHAR, "reviewer_email" VARCHAR, "comment" VARCHAR, "rating" INTEGER, PRIMARY KEY (reviewee_email, reviewer_email))'
+    const sql = 'create table if not exists "reviews"("reviewee_email" varchar, "reviewer_email" varchar, "comment" varchar, "rating" integer, primary key (reviewee_email, reviewer_email))'
     const tableInitialized = pool.query(sql).then(()=> {
         console.log("Database connection established")
     })
 
     this.getAll = async function () {
-        await tableInitialized;
+        await tableInitialized
         const result = await pool.query('select * from "reviews"')
         return result.rows
     }
 
     this.getAllFor = async function (revieweeEmail) {
-        await tableInitialized;
+        await tableInitialized
         const result = await pool.query('select * from "reviews" where reviewee_email = $1', [revieweeEmail])
         return result.rows
     }
 
     this.getAverageRating = async function (revieweeEmail) {
-        await tableInitialized;
+        await tableInitialized
         const result = await pool.query(`select avg(rating) as "average_rating" from "reviews" where "reviewee_email" = '${revieweeEmail}'`)
         return result.rows[0]
     }
 
     this.create = async function (review) {
-        await tableInitialized;
+        await tableInitialized
         const query = 'insert into "reviews" ("reviewee_email", "reviewer_email", "rating", "comment") values ($1, $2, $3, $4)'
         const values = [review.reviewee_email, review.reviewer_email, review.rating, review.comment]
         await pool.query(query, values)
     }
 
     this.deleteAll = async function () {
-        await tableInitialized;
+        await tableInitialized
         await pool.query('delete from "reviews"')
     }
 
