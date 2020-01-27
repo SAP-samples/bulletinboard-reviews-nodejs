3a30aa15e75bd8d094e19bd099b38f0659d42d51
D068017 <marcel.dambach@sap.com>
2020-01-27 11:31:35 +0100

    Use vcapservices if available

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
 
 const ExpressServer = require('./express-server')
 const PostgresReviewsService = require('./postgres-reviews-service')
+const VCAP_SERVICES = process.env.VCAP_SERVICES
+let DB_CONNECTION_URI  = process.env.POSTGRES_URI
 
-// const VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES)
-// const DB_CONNECTION_URI = VCAP_SERVICES.postgresql[0].credentials.uri
-const DB_CONNECTION_URI = process.env.POSTGRES_URI
+if (VCAP_SERVICES) {
+    const vcapServices = JSON.parse(VCAP_SERVICES)
+    DB_CONNECTION_URI = vcapServices.postgresql[0].credentials.uri
+}
 
 const server = new ExpressServer(new PostgresReviewsService(DB_CONNECTION_URI))
 
