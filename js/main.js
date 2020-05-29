2b4152b76859cf5ecd0309c28cc8c0b4dac72a47
Moritz Schoenwandt <moritz.schoenwandt@sap.com>
2020-05-29 14:34:41 +0200

    minor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
-
 const ExpressServer = require('./express-server')
 const PostgresReviewsService = require('./postgres-reviews-service')
+
 const VCAP_SERVICES = process.env.VCAP_SERVICES
 const DB_CONNECTION_URI = process.env.POSTGRES_URI || "postgres://postgres@localhost:6543/postgres"
 const PORT_DEFAULT = 9090
@@ -11,6 +11,7 @@ if (VCAP_SERVICES) {
     const vcapServices = JSON.parse(VCAP_SERVICES)
     dbConnectionUriVCAP = vcapServices.postgresql[0].credentials.uri
 }
+
 const server = new ExpressServer(new PostgresReviewsService(
     dbConnectionUriVCAP || DB_CONNECTION_URI
 ))
