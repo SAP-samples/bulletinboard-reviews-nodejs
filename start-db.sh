4d3c11417e6f8b448a3a8c3ba4d7670f3bc7371d
Moritz Schoenwandt <moritz.schoenwandt@sap.com>
2020-05-29 14:34:29 +0200

    no absolute path for windows

--- start-db.sh
+++ start-db.sh
@@ -6,7 +6,7 @@ then
     echo "DB container doesn't exist, creating"
     docker run -d -p 6543:5432 --name postgres-bulletinboard-reviews -e POSTGRES_HOST_AUTH_METHOD=trust postgres:9.6-alpine
     docker exec -t postgres-bulletinboard-reviews sh -c "until pg_isready -q -U postgres -h localhost; do echo waiting for database; sleep 2; done;"
-    docker exec -t postgres-bulletinboard-reviews /usr/local/bin/psql -q -U postgres -h localhost -c "CREATE DATABASE testdb"
+    docker exec -t postgres-bulletinboard-reviews psql -q -U postgres -h localhost -c "CREATE DATABASE testdb"
     echo "DB container running"
 else
     is_db_container_running=`docker inspect -f '{{.State.Running}}' postgres-bulletinboard-reviews`
